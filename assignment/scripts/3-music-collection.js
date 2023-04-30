c27a36f0baa638489f8bfb5f3fefd8a97dadedad
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-04-30 17:12:59 -0500

    Made the code cleaner

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,43 +1,49 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
 
 let Collection = [];
 
-function addToCollection(title , artist , yearPublished){
-    let album = {
-        title: title ,
-        artist: artist ,
-        yearPublished: yearPublished ,
-    }
-    Collection.push(album) ;
-return album;
-};
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  Collection.push(album);
+  return album;
+}
 
+console.log(addToCollection("My Turn", "Lil Baby", 2020));
+console.log(addToCollection("Tha Carter V", "Lil Wayne", 2018));
+console.log(addToCollection("Free 6lack", "6lack", 2016));
+console.log(addToCollection("Hall Of Fame", "Polo G", 2021));
+console.log(
+  addToCollection("Please Excuse Me For Being Antisocial", "Roddy Ricch", 2019)
+);
+console.log(addToCollection("Abbey Road", "The Beatles", 1969));
 
-console.log(addToCollection('My Turn', 'Lil Baby', 2020));
-console.log(addToCollection('Tha Carter V' , 'Lil Wayne', 2018));
-console.log(addToCollection('Free 6lack', '6lack', 2016));
-console.log(addToCollection('Hall Of Fame' ,'Polo G', 2021));
-console.log(addToCollection('Please Excuse Me For Being Antisocial', 'Roddy Ricch', 2019));
-console.log(addToCollection('Abbey Road' ,'The Beatles', 1969));
-
-function showCollection(){
-    console.log('My favorite albums: ' , Collection.length)
-    for (i = 0 ; i < Collection.length ; i++)
-    console.log(Collection[i].title, 'by' , Collection[i].artist, 'published in year' , Collection[i].yearPublished);
-
-};
+function showCollection() {
+  console.log("My favorite albums: ", Collection.length);
+  for (i = 0; i < Collection.length; i++)
+    console.log(
+      Collection[i].title,
+      "by",
+      Collection[i].artist,
+      "published in year",
+      Collection[i].yearPublished
+    );
+}
 
 showCollection(Collection);
 
-function findByArtist(artist){
-    let searchArray = [];
-    for( let i = 0; i<Collection.length; i++){
-        if( Collection[i].artist === artist){
-            searchArray.push(Collection[i]);
-        }
+function findByArtist(artist) {
+  let searchArray = [];
+  for (let i = 0; i < Collection.length; i++) {
+    if (Collection[i].artist === artist) {
+      searchArray.push(Collection[i]);
     }
-    return searchArray;
+  }
+  return searchArray;
 }
 
-console.log(findByArtist('Drake'));
-console.log(findByArtist('6lack'));
+console.log(findByArtist("Drake"));
+console.log(findByArtist("6lack"));
